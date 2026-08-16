e9782d746eb012dee31f94aca60a21d1ea1f7eaf
rafreo21 <rafreo21@gmail.com>
2026-08-16 12:54:12 +0100

    Fix Vercel error-boundary server crash

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    if (typeof window !== "undefined") {
+      void import("@sentry/nextjs").then((Sentry) => {
+        Sentry.captureException(error);
+      }).catch(() => {});
+    }
   }, [error]);
 
   return (
